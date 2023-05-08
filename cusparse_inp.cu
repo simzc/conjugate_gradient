60ec0aecfb33e20a2677fa55a6a689619efac886
simzc <60860967+simzc@users.noreply.github.com>
2023-05-08 10:54:52 +0800

    added the function demo converts dense to sparse

--- cusparse_inp.cu
+++ cusparse_inp.cu
@@ -1,253 +1,188 @@
-// includes, system
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*
+ * Copyright 1993-2022 NVIDIA Corporation.  All rights reserved.
+ *
+ * NOTICE TO LICENSEE:
+ *
+ * This source code and/or documentation ("Licensed Deliverables") are
+ * subject to NVIDIA intellectual property rights under U.S. and
+ * international Copyright laws.
+ *
+ * These Licensed Deliverables contained herein is PROPRIETARY and
+ * CONFIDENTIAL to NVIDIA and is being provided under the terms and
+ * conditions of a form of NVIDIA software license agreement by and
+ * between NVIDIA and Licensee ("License Agreement") or electronically
+ * accepted by Licensee.  Notwithstanding any terms or conditions to
+ * the contrary in the License Agreement, reproduction or disclosure
+ * of the Licensed Deliverables to any third party without the express
+ * written consent of NVIDIA is prohibited.
+ *
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, NVIDIA MAKES NO REPRESENTATION ABOUT THE
+ * SUITABILITY OF THESE LICENSED DELIVERABLES FOR ANY PURPOSE.  IT IS
+ * PROVIDED "AS IS" WITHOUT EXPRESS OR IMPLIED WARRANTY OF ANY KIND.
+ * NVIDIA DISCLAIMS ALL WARRANTIES WITH REGARD TO THESE LICENSED
+ * DELIVERABLES, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE.
+ * NOTWITHSTANDING ANY TERMS OR CONDITIONS TO THE CONTRARY IN THE
+ * LICENSE AGREEMENT, IN NO EVENT SHALL NVIDIA BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THESE LICENSED DELIVERABLES.
+ *
+ * U.S. Government End Users.  These Licensed Deliverables are a
+ * "commercial item" as that term is defined at 48 C.F.R. 2.101 (OCT
+ * 1995), consisting of "commercial computer software" and "commercial
+ * computer software documentation" as such terms are used in 48
+ * C.F.R. 12.212 (SEPT 1995) and is provided to the U.S. Government
+ * only as a commercial end item.  Consistent with 48 C.F.R.12.212 and
+ * 48 C.F.R. 227.7202-1 through 227.7202-4 (JUNE 1995), all
+ * U.S. Government End Users acquire the Licensed Deliverables with
+ * only those rights set forth herein.
+ *
+ * Any use of the Licensed Deliverables in individual and commercial
+ * software must include, in the user documentation and internal
+ * comments to the code, the above Disclaimer and U.S. Government End
+ * Users Notice.
+ */
+#include <cuda_runtime_api.h> // cudaMalloc, cudaMemcpy, etc.
+#include <cusparse.h>         // cusparseSparseToDense
+#include <stdio.h>            // printf
+#include <stdlib.h>           // EXIT_FAILURE
 
-/* Using updated (v2) interfaces to cublas */
-#include <cublas_v2.h>
-#include <cuda_runtime.h>
-#include <cusparse.h>
-
-// #include "cuda_pipeline_helpers.h"
-// // Utilities and system includes
-#include <helper_cuda.h>      // helper function CUDA error checking and initialization
-#include <helper_functions.h> // helper for shared functions common to CUDA Samples
-
-const char *sSDKname = "conjugateGradient";
-
-/* genTridiag: generate a random tridiagonal symmetric matrix */
-void genTridiag(int *I, int *J, float *val, int N, int nz)
-{
-    I[0] = 0, J[0] = 0, J[1] = 1;
-    val[0] = (float)rand() / RAND_MAX + 10.0f;
-    val[1] = (float)rand() / RAND_MAX;
-    int start;
-
-    for (int i = 1; i < N; i++)
-    {
-        if (i > 1)
-        {
-            I[i] = I[i - 1] + 3;
-        }
-        else
-        {
-            I[1] = 2;
-        }
-
-        start = (i - 1) * 3 + 2;
-        J[start] = i - 1;
-        J[start + 1] = i;
-
-        if (i < N - 1)
-        {
-            J[start + 2] = i + 1;
-        }
-
-        val[start] = val[start - 1];
-        val[start + 1] = (float)rand() / RAND_MAX + 10.0f;
-
-        if (i < N - 1)
-        {
-            val[start + 2] = (float)rand() / RAND_MAX;
-        }
-    }
-
-    I[N] = nz;
+#define CHECK_CUDA(func)                                                        \
+{                                                                              \
+    cudaError_t status = (func);                                               \
+    if (status != cudaSuccess) {                                               \
+        printf("CUDA API failed at line %d with error: %s (%d)\n",             \
+               __LINE__, cudaGetErrorString(status), status);                  \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
 }
 
-int main(int argc, char **argv)
-{
-    int M = 0, N = 0, nz = 0, *I = NULL, *J = NULL;
-    float *val = NULL;
-    const float tol = 1e-5f;
-    const int max_iter = 10000;
-    float *x;
-    float *rhs;
-    float a, b, na, r0, r1;
-    int *d_col, *d_row;
-    float *d_val, *d_x, dot;
-    float *d_r, *d_p, *d_Ax;
-    int k;
-    float alpha, beta, alpham1;
+#define CHECK_CUSPARSE(func)                                                    \
+{                                                                              \
+    cusparseStatus_t status = (func);                                          \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                   \
+        printf("CUSPARSE API failed at line %d with error: %s (%d)\n",         \
+               __LINE__, cusparseGetErrorString(status), status);              \
+        return EXIT_FAILURE;                                                   \
+    }                                                                          \
+}
 
-    // This will pick the best possible CUDA capable device
-    cudaDeviceProp deviceProp;
-    int devID = findCudaDevice(argc, (const char **)argv);
-
-    if (devID < 0)
-    {
-        printf("exiting...\n");
-        exit(EXIT_SUCCESS);
-    }
-
-    checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
-
-    // Statistics about the GPU device
-    printf(
-        "> GPU device has %d Multi-Processors, SM %d.%d compute capabilities\n\n",
-        deviceProp.multiProcessorCount, deviceProp.major, deviceProp.minor);
-
-    /* Generate a random tridiagonal symmetric matrix in CSR format */
-    M = N = 1048576;
-    nz = (N - 2) * 3 + 4;
-    I = (int *)malloc(sizeof(int) * (N + 1));
-    J = (int *)malloc(sizeof(int) * nz);
-    val = (float *)malloc(sizeof(float) * nz);
-    genTridiag(I, J, val, N, nz);
-
-    x = (float *)malloc(sizeof(float) * N);
-    rhs = (float *)malloc(sizeof(float) * N);
-
-    for (int i = 0; i < N; i++)
-    {
-        rhs[i] = 1.0;
-        x[i] = 0.0;
-    }
-
-    /* Get handle to the CUBLAS context */
-    cublasHandle_t cublasHandle = 0;
-    cublasStatus_t cublasStatus;
-    cublasStatus = cublasCreate(&cublasHandle);
-
-    checkCudaErrors(cublasStatus);
-
-    /* Get handle to the CUSPARSE context */
-    cusparseHandle_t cusparseHandle = 0;
-    checkCudaErrors(cusparseCreate(&cusparseHandle));
-
-    checkCudaErrors(cudaMalloc((void **)&d_col, nz * sizeof(int)));
-    checkCudaErrors(cudaMalloc((void **)&d_row, (N + 1) * sizeof(int)));
-    checkCudaErrors(cudaMalloc((void **)&d_val, nz * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **)&d_x, N * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **)&d_r, N * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **)&d_p, N * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **)&d_Ax, N * sizeof(float)));
-
-    /* Wrap raw data into cuSPARSE generic API objects */
-    cusparseSpMatDescr_t matA = NULL;
-    checkCudaErrors(cusparseCreateCsr(&matA, N, N, nz, d_row, d_col, d_val,
+int main(void) {
+    // Host problem definition
+    int   num_rows   = 5;
+    int   num_cols   = 4;
+    int   ld         = num_cols;
+    int   dense_size = ld * num_rows;
+    float  h_dense[]  = { 1.0f,  0.0f,  2.0f,  3.0f,
+                         0.0f,  4.0f,  0.0f,  0.0f,
+                         5.0f,  0.0f,  6.0f,  7.0f,
+                         0.0f,  8.0f,  0.0f,  9.0f,
+                         0.0f, 10.0f, 11.0f,  0.0f };
+    int  h_csr_offsets[]         = { 0, 0, 0, 0, 0, 0  };
+    int  h_csr_columns[]         = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    float h_csr_values[]          = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int  h_csr_offsets_result[]  = { 0, 3, 4, 7, 9, 11 };
+    int  h_csr_columns_result[]  = { 0, 2, 3, 1, 0, 2, 3, 1, 3, 1, 2 };
+    float h_csr_values_result[]   = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f,
+                                     7.0f, 8.0f, 9.0f, 10.0f, 11.0f };
+    //--------------------------------------------------------------------------
+    // Device memory management
+    int   *d_csr_offsets, *d_csr_columns;
+    float *d_csr_values,  *d_dense;
+    CHECK_CUDA( cudaMalloc((void**) &d_dense, dense_size * sizeof(float)))
+    CHECK_CUDA( cudaMalloc((void**) &d_csr_offsets,
+                           (num_rows + 1) * sizeof(int)) )
+    CHECK_CUDA( cudaMemcpy(d_dense, h_dense, dense_size * sizeof(float),
+                           cudaMemcpyHostToDevice) )
+    //--------------------------------------------------------------------------
+    // CUSPARSE APIs
+    cusparseHandle_t     handle = NULL;
+    cusparseSpMatDescr_t matB;
+    cusparseDnMatDescr_t matA;
+    void*                dBuffer    = NULL;
+    size_t               bufferSize = 0;
+    CHECK_CUSPARSE( cusparseCreate(&handle) )
+    // Create dense matrix A
+    CHECK_CUSPARSE( cusparseCreateDnMat(&matA, num_rows, num_cols, ld, d_dense,
+                                        CUDA_R_32F, CUSPARSE_ORDER_ROW) )
+    // Create sparse matrix B in CSR format
+    CHECK_CUSPARSE( cusparseCreateCsr(&matB, num_rows, num_cols, 0,
+                                      d_csr_offsets, NULL, NULL,
                                       CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I,
-                                      CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F));
-    cusparseDnVecDescr_t vecx = NULL;
-    checkCudaErrors(cusparseCreateDnVec(&vecx, N, d_x, CUDA_R_32F));
-    cusparseDnVecDescr_t vecp = NULL;
-    checkCudaErrors(cusparseCreateDnVec(&vecp, N, d_p, CUDA_R_32F));
-    cusparseDnVecDescr_t vecAx = NULL;
-    checkCudaErrors(cusparseCreateDnVec(&vecAx, N, d_Ax, CUDA_R_32F));
+                                      CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F) )
 
-    /* Initialize problem data */
-    cudaMemcpy(d_col, J, nz * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_row, I, (N + 1) * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_val, val, nz * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_x, x, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_r, rhs, N * sizeof(float), cudaMemcpyHostToDevice);
+    // allocate an external buffer if needed
+    CHECK_CUSPARSE( cusparseDenseToSparse_bufferSize(
+                                        handle, matA, matB,
+                                        CUSPARSE_DENSETOSPARSE_ALG_DEFAULT,
+                                        &bufferSize) )
+    CHECK_CUDA( cudaMalloc(&dBuffer, bufferSize) )
 
-    alpha = 1.0;
-    alpham1 = -1.0;
-    beta = 0.0;
-    r0 = 0.;
+    // execute Sparse to Dense conversion
+    CHECK_CUSPARSE( cusparseDenseToSparse_analysis(handle, matA, matB,
+                                        CUSPARSE_DENSETOSPARSE_ALG_DEFAULT,
+                                        dBuffer) )
+    // get number of non-zero elements
+    int64_t num_rows_tmp, num_cols_tmp, nnz;
+    CHECK_CUSPARSE( cusparseSpMatGetSize(matB, &num_rows_tmp, &num_cols_tmp,
+                                         &nnz) )
 
-    /* Allocate workspace for cuSPARSE */
-    size_t bufferSize = 0;
-    checkCudaErrors(cusparseSpMV_bufferSize(
-        cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA, vecx,
-        &beta, vecAx, CUDA_R_32F, CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize));
-    void *buffer = NULL;
-    checkCudaErrors(cudaMalloc(&buffer, bufferSize));
-
-    /* Begin CG */
-    checkCudaErrors(cusparseSpMV(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                 &alpha, matA, vecx, &beta, vecAx, CUDA_R_32F,
-                                 CUSPARSE_SPMV_ALG_DEFAULT, buffer));
-
-    cublasSaxpy(cublasHandle, N, &alpham1, d_Ax, 1, d_r, 1);
-    cublasStatus = cublasSdot(cublasHandle, N, d_r, 1, d_r, 1, &r1);
-
-    k = 1;
-
-    while (r1 > tol * tol && k <= max_iter)
-    {
-        if (k > 1)
-        {
-            b = r1 / r0;
-            cublasStatus = cublasSscal(cublasHandle, N, &b, d_p, 1);
-            cublasStatus = cublasSaxpy(cublasHandle, N, &alpha, d_r, 1, d_p, 1);
-        }
-        else
-        {
-            cublasStatus = cublasScopy(cublasHandle, N, d_r, 1, d_p, 1);
-        }
-
-        checkCudaErrors(cusparseSpMV(
-            cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha, matA, vecp,
-            &beta, vecAx, CUDA_R_32F, CUSPARSE_SPMV_ALG_DEFAULT, buffer));
-        cublasStatus = cublasSdot(cublasHandle, N, d_p, 1, d_Ax, 1, &dot);
-        a = r1 / dot;
-
-        cublasStatus = cublasSaxpy(cublasHandle, N, &a, d_p, 1, d_x, 1);
-        na = -a;
-        cublasStatus = cublasSaxpy(cublasHandle, N, &na, d_Ax, 1, d_r, 1);
-
-        r0 = r1;
-        cublasStatus = cublasSdot(cublasHandle, N, d_r, 1, d_r, 1, &r1);
-        cudaDeviceSynchronize();
-        printf("iteration = %3d, residual = %e\n", k, sqrt(r1));
-        k++;
-    }
-
-    cudaMemcpy(x, d_x, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-    float rsum, diff, err = 0.0;
-
-    for (int i = 0; i < N; i++)
-    {
-        rsum = 0.0;
-
-        for (int j = I[i]; j < I[i + 1]; j++)
-        {
-            rsum += val[j] * x[J[j]];
-        }
-
-        diff = fabs(rsum - rhs[i]);
-
-        if (diff > err)
-        {
-            err = diff;
+    // allocate CSR column indices and values
+    CHECK_CUDA( cudaMalloc((void**) &d_csr_columns, nnz * sizeof(int))   )
+    CHECK_CUDA( cudaMalloc((void**) &d_csr_values,  nnz * sizeof(float)) )
+    // reset offsets, column indices, and values pointers
+    CHECK_CUSPARSE( cusparseCsrSetPointers(matB, d_csr_offsets, d_csr_columns,
+                                           d_csr_values) )
+    // execute Sparse to Dense conversion
+    CHECK_CUSPARSE( cusparseDenseToSparse_convert(handle, matA, matB,
+                                        CUSPARSE_DENSETOSPARSE_ALG_DEFAULT,
+                                        dBuffer) )
+    // destroy matrix/vector descriptors
+    CHECK_CUSPARSE( cusparseDestroyDnMat(matA) )
+    CHECK_CUSPARSE( cusparseDestroySpMat(matB) )
+    CHECK_CUSPARSE( cusparseDestroy(handle) )
+    //--------------------------------------------------------------------------
+    // device result check
+    CHECK_CUDA( cudaMemcpy(h_csr_offsets, d_csr_offsets,
+                           (num_rows + 1) * sizeof(int),
+                           cudaMemcpyDeviceToHost) )
+    CHECK_CUDA( cudaMemcpy(h_csr_columns, d_csr_columns, nnz * sizeof(int),
+                           cudaMemcpyDeviceToHost) )
+    CHECK_CUDA( cudaMemcpy(h_csr_values, d_csr_values, nnz * sizeof(float),
+                           cudaMemcpyDeviceToHost) )
+    int correct = 1;
+    for (int i = 0; i < num_rows + 1; i++) {
+        if (h_csr_offsets[i] != h_csr_offsets_result[i]) {
+            correct = 0;
+            break;
         }
     }
-
-    cusparseDestroy(cusparseHandle);
-    cublasDestroy(cublasHandle);
-    if (matA)
-    {
-        checkCudaErrors(cusparseDestroySpMat(matA));
+    for (int i = 0; i < nnz; i++) {
+        if (h_csr_columns[i] != h_csr_columns_result[i]) {
+            correct = 0;
+            break;
+        }
     }
-    if (vecx)
-    {
-        checkCudaErrors(cusparseDestroyDnVec(vecx));
+    for (int i = 0; i < nnz; i++) {
+        if (h_csr_values[i] != h_csr_values_result[i]) {
+            correct = 0;
+            break;
+        }
     }
-    if (vecAx)
-    {
-        checkCudaErrors(cusparseDestroyDnVec(vecAx));
-    }
-    if (vecp)
-    {
-        checkCudaErrors(cusparseDestroyDnVec(vecp));
-    }
-
-    free(I);
-    free(J);
-    free(val);
-    free(x);
-    free(rhs);
-    cudaFree(d_col);
-    cudaFree(d_row);
-    cudaFree(d_val);
-    cudaFree(d_x);
-    cudaFree(d_r);
-    cudaFree(d_p);
-    cudaFree(d_Ax);
-
-    printf("Test Summary:  Error amount = %f\n", err);
-    exit((k <= max_iter) ? 0 : 1);
+    if (correct)
+        printf("dense2sparse_csr_example test PASSED\n");
+    else
+        printf("dense2sparse_csr_example test FAILED: wrong result\n");
+    //--------------------------------------------------------------------------
+    // device memory deallocation
+    CHECK_CUDA( cudaFree(dBuffer) )
+    CHECK_CUDA( cudaFree(d_csr_offsets) )
+    CHECK_CUDA( cudaFree(d_csr_columns) )
+    CHECK_CUDA( cudaFree(d_csr_values) )
+    CHECK_CUDA( cudaFree(d_dense) )
+    return EXIT_SUCCESS;
 }
